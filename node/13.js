421d6496f2069a541defc88c0ddc8559b86c532e
zhengchunguang <1083466294@qq.com>
2018-12-04 18:32:33 +0800

    1223333

--- node/13.js
+++ node/13.js
@@ -4,3 +4,15 @@
 // console.log(decodeURIComponent(b));
 
 // 用于处理get请求特殊字符时候解析问题
+
+// 数组find方法
+
+// let arr=[
+// 	{id:1,name:"张三"},
+// 	{id:2,name:"李四"},
+// 	{id:3,name:"王五"}
+// ]
+
+// let arrOne=arr.find(item=>item.id==2);
+
+// console.log(arrOne)  //{id:2,name:"李四"}
